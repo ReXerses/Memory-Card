a829a401b3c37162ccab90f74cd5c8e2c38382da
ReXerses <108536533+ReXerses@users.noreply.github.com>
2023-12-26 22:48:49 +0100

    1)Aggiunti font e cursor custom. 2)Aggiunto video in loop (muted) come background della pagina. 3)Aggiunto audio in loop (volume 0.1).

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
-function App() {
 
+function App() {
+  useEffect(() => {
+    // Una volta che la pagina è stata caricata
+    const audioElement = document.getElementById('myAudio');
+    if (audioElement) {
+      audioElement.volume = 0.1; // Imposta il volume desiderato (da 0.0 a 1.0)
+    }
+  }, []);
 
   return (
     <>
-      
+      <h1 className='title' >Lovecraftian Memory Card Game</h1>
+      <audio autoPlay loop id="myAudio">
+        <source src="/sound.mp3" type="audio/mp3" />
+        Il tuo browser non supporta l'elemento audio.
+      </audio>
+      <video autoPlay muted loop id="myVideo">
+        <source src="/background.mp4" type="video/mp4" />
+      </video>
+
+      <footer>
+
+        <a href="https://github.com/ReXerses" target="_blank">
+            <img src="/github.svg" alt="Personal Github link"/>
+        </a>
+        <a class='nome-github' href="https://github.com/ReXerses" target="_blank">ReXerses</a>
+
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
